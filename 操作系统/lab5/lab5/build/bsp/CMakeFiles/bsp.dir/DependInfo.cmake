cf157703034b8ca39bada68e006c0638619b1b9d
YUYI-0009 <w163_092909@163.com>
2025-07-09 10:29:21 +0800

    Add .gitattributes file

---
+++ 操作系统/lab5/lab5/build/bsp/CMakeFiles/bsp.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/wang/opreatingSystem/lab5/src/bsp/prt_reset_vector.S" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/prt_reset_vector.S.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/prt_vector.S" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/prt_vector.S.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/start.S" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/start.S.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/wang/opreatingSystem/lab5/src/include"
+  "/home/wang/opreatingSystem/lab5/src/bsp"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wang/opreatingSystem/lab5/src/bsp/hwi_init.c" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/hwi_init.c.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/print.c" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/print.c.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/prt_exc.c" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/prt_exc.c.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/timer.c" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/timer.c.obj"
+  "/home/wang/opreatingSystem/lab5/src/bsp/vsnprintf_s.c" "/home/wang/opreatingSystem/lab5/build/bsp/CMakeFiles/bsp.dir/vsnprintf_s.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/wang/opreatingSystem/lab5/src/include"
+  "/home/wang/opreatingSystem/lab5/src/bsp"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
